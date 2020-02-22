26a030228a498b673389764a86d07f10a70ebf2a
Anartz Mugika Ledo <mugan86@gmail.com>
2020-02-22 18:49:38 +0100

    Añadido npm ignore

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <CustomBox content="Anartz" bgColor="blue" width="400px" height="300px"/>
+        <CustomBox content="Anartz Mugika Ledo" bgColor="red" width="200px" height="90px"/>
+        <br/>
+        <CustomBox content="Anartz" bgColor="blue" width="250px" height="100px"/>
+        <br/>
+        <CustomBox/>
         <p>
           Anartz Mugika Ledo - mugan86@gmail.com.
         </p>
